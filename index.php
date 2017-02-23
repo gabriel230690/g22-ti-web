123064e1111f36064bd275dbb66cebcbc9e4492a
Gabriel <gabriel230690@gmail.com>
2017-02-23 20:23:45 -0300

    * Testes deploy

--- index.php
+++ index.php
@@ -31,8 +31,36 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+
+    <!-- BEGIN JIVOSITE CODE {literal} -->
+    <script type='text/javascript'>
+        (function () {
+            var widget_id = 'IhiLanaMbj';
+            var d = document;
+            var w = window;
+
+            function l() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = '//code.jivosite.com/script/widget/' + widget_id;
+                var ss = document.getElementsByTagName('script')[0];
+                ss.parentNode.insertBefore(s, ss);
+            }
+
+            if (d.readyState == 'complete') {
+                l();
+            } else {
+                if (w.attachEvent) {
+                    w.attachEvent('onload', l);
+                } else {
+                    w.addEventListener('load', l, false);
+                }
+            }
+        })();</script>
+    <!-- {/literal} END JIVOSITE CODE -->
 </head>
+
 
 <body data-spy="scroll">
 <!-- ******HEADER****** -->
@@ -422,7 +450,6 @@
     </div>
 </footer>
 <!--//footer-->
-
 
 
 <!-- Javascript -->
