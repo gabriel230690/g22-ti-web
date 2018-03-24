2e17c3221df85fd32dafae3c57d1a54f0111eb5c
Jonathan Cristiano da Silva <jonathancsilva@gmail.com>
2018-03-24 10:57:14 -0300

    Correção no endereço de e-mail

--- index.php
+++ index.php
@@ -542,7 +542,7 @@
             <div class="intro col-md-6 col-sm-12 col-xs-12 col-md-offset-3">
                 <p>Tem interesse em nossos serviços? Faça seu orçamento sem compromisso agora mesmo.</p>
                 <ul class="list-unstyled contact-details">
-                    <li><i class="fa fa-envelope"></i><a href="mailto:hello@website.com">contato@g22ti.com.br</a></li>
+                    <li><i class="fa fa-envelope"></i><a href="mailto:contato@g22ti.com.br">contato@g22ti.com.br</a></li>
                     <li><i class="fa fa-phone-square"></i>(47) 99194-4696</li>
                 </ul>
             </div>
