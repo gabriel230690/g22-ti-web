306894c82b95f0a96244afea8d1b9dfe06f91fa2
Gabriel <gabriel230690@gmail.com>
2018-07-27 21:14:58 -0300

    HTTPS

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <!--[if !IE]><!-->
 <html lang="en">
 <head>
-    <title>G22 TI - Delivery</title>
+    <title>G22 TI - Plataforma de Delivery</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
